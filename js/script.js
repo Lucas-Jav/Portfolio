e93e09cee1efd41d83ab4b4b5e47d23dd3a588b9
Lucas <lucasrazebra@gmail.com>
2022-10-01 10:17:49 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const inputDayOrNight = document.querySelector('input[name=checkbox]')
+
+inputDayOrNight.addEventListener('change', function() {
+    const trocaCor = document.getElementById('colordays')
+
+    if (this.checked) {
+        trocaCor.classList.remove('day')
+        trocaCor.classList.add('nigth')
+    } else {
+        trocaCor.classList.remove('nigth')
+        trocaCor.classList.add('day')
+    }
+})
+
+function buttonCv() {
+    window.open('../DOM/cv.pdf', '_blank');
+}
